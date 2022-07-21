27f1dcde69bbe8ec2d7cf157879b7ca7d8296efc
IK <95004425+IK-20211125@users.noreply.github.com>
2022-07-21 11:25:10 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@ $(function () {
     $("h1").textillate();
 });
 
+document.getElementById("date").innerHTML = getNow();
+
+function getNow() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var mon = now.getMonth() + 1;
+    var day = now.getDate();
+   
+    var nowDate = year + "/" + mon + "/" + day;
+
+    return nowDate;
+}
+
